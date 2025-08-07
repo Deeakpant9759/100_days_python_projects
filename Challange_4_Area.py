39edaec9779bdd33a161bbbc75bc9584cf263db5
Deepak pant <deepakpant9759@gmail.com>
2025-08-07 10:13:50 +0530

    updated the things

---
+++ Challange_4_Area.py
@@ -0,0 +1,19 @@
+class Area():
+    def __init__(self,Base,Length,Height):
+        self.Base = Base
+        self.Length = Length
+        self.Height = Height
+    def is_Valid(self):
+            if not all(isinstance(x,(int,float)) for x in [self.Base,self.Length,self.Height]):
+                raise TypeError("Values Can't be an Float")
+            if not (self.Base >= 0 and self.Length >=0 and self.Height >=0):
+                raise ValueError ("Measure Ment Can't Negtive")
+            
+        
+                
+
+
+
+    def Tringle(self):
+
+        
