a84472f8f94bfedb2e6b5be3e91b310743fc4dca
Deepakpant9759 <deepakpant9759@gmail.com>
2025-08-11 22:02:09 +0530

    new things

---
+++ momorization_febonicci.py
@@ -0,0 +1,7 @@
+def fabonacci(n,ht={0:0,1:1}):
+    if n in ht :
+        return ht[n]
+    else:
+        ht[n] = fabonacci(n-1)+fabonacci(n-2)
+        return ht[n]
+fabonacci(4)
--- n_fibonacci.py
+++ n_fibonacci.py
@@ -5,5 +5,4 @@ def fibonacci_Sol(n):
         return 1
     number = fibonacci_Sol(n-1) + fibonacci_Sol(n-2)
     return number
-    
 print(fibonacci_Sol(50))
